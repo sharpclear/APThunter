bdf5579d606bff56af0506afd40fe012666d5b79
sharpclear <malizhao@stu.scu.edu.cn>
2025-10-28 15:58:52 +0800

    initial

---
+++ src/pages/dashboard/attributes/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineOptions({ name: 'DashboardAttributes' })
+</script>
+
+<template>
+  <page-container>
+    <a-card :bordered="false">
+      <h2>属性分析</h2>
+      <div>在此处添加你的图表或数据展示组件</div>
+    </a-card>
+  </page-container>
+</template>
+
+<style scoped>
+h2 {
+  margin: 0 0 16px 0;
+}
+</style>
